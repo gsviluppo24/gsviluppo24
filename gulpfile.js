8be798d0a19bc92220b56f657bc09257316703e5
Cyril Dally <c.dally@anoki.it>
2015-09-23 10:39:41 +0200

    Added debug option and fixed dryrun option + Added documentation on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
+/**
+ * Opzioni disponibili:
+ *
+ * --env: string - Definisce l'ambiente di deploy - Default: staging
+ * -- branch: string - Definisce il branch di git - Default: develop
+ * -- dryrun: boolean - Se settato su false, effettua il deploy, altrimenti solo prova - Default: true
+ * -- debug: boolean - Se settato su true, la console stamperà degli elementi di controllo -  Default: false
+ * 
+ * @author Cyril Dally
+ * @data: 2015-09-23
+ * 
+ */
+
 var gulp = require('gulp');
 var os   = require('os');
 var gulpif = require('gulp-if');
@@ -14,7 +27,7 @@ var sass = require('gulp-sass');
 
 var knownOptions = {
   string: 'env',
-  default: { env: process.env.NODE_ENV || 'develop' }
+  default: { env: process.env.NODE_ENV || 'staging' }
 };
 
 // Crea un array unico
@@ -32,7 +45,6 @@ gulp.task( 'config', function() {
   // }
 
   // Se non specificato l'env viene settato su develop
-  // -> staging
   env = undefined == options.env ? 'staging' : options.env;
 
   // Se non specificato il branch viene settato su develop
@@ -44,7 +56,10 @@ gulp.task( 'config', function() {
   }
 
   // Se non specificato il dryrun viene settato su false
-  dryrun = undefined == options.dryrun ? false : options.dryrun;
+  dryrun = undefined == options.dryrun ? true : options.dryrun;
+
+  // Se non specificato il debug viene settato su false
+  debug = undefined == options.debug ? false : options.debug;
 
   // Di default il disco di destinazione è settato su quello di dev
   // Non è un'opzione modificabile
@@ -55,15 +70,17 @@ gulp.task( 'config', function() {
     branch = 'master';
     url = 'disco/di/produzione/htdocs/';
 
-    // TODO: Riccardo spiegami questo
+    // Questo genera un errore: permessi ?
     // fs.createReadStream('.env.prod').pipe( fs.createWriteStream('.env') );
   }
 
-  // console.log( "hostname = " + hostname );
-  // console.log( "env = " + env );
-  // console.log( "branch = " + branch );
-  // console.log( "url = " + url );
-  // console.log( "dryrun = " + dryrun );
+  if( debug ) {
+    console.log( "Task config: hostname = " + hostname );
+    console.log( "Task config: env = " + env );
+    console.log( "Task config: branch = " + branch );
+    console.log( "Task config: url = " + url );
+    console.log( "Task config: dryrun = " + dryrun );
+  }
 });
 
 
@@ -71,10 +88,12 @@ gulp.task( 'config', function() {
 // Depends on config
 gulp.task('git-pull', ['config'], function() {
 
-  // console.log( "branch = " + branch );
+  if( debug ) {
+    console.log( "Task git-pull: branch = " + branch );
+  }
 
   // checkout: verificare sintassi
-  git.checkout('origin', branch, function (err) {
+  git.checkout(branch, {}, function (err) {
     if (err) throw err;
   });
 
@@ -97,33 +116,34 @@ gulp.task( 'clean', ['config'], function () {
 // Depends on clean
 gulp.task( 'cssmin', ['clean'], function() {
 
+  if( debug ) {
+    console.log( "Task cssmin: env = " + env );
+  }
+
   return gulp.src( 'source/css/*.css' )
-    .pipe(gulpif( options.env == "production", minifyCss() )) // only minify in production
+    .pipe(gulpif( env == "production", minifyCss() )) // only minify in production
     .pipe(gulp.dest('css'));
 });
-
-
-
-
-
-
 
 gulp.task('sync', function() {
 
   var file = fs.readFileSync('rsync-excludelist', "utf8");
   var arr = file.split("\n");
 
+  if( debug ) {
+    console.log( "Task sync: arr = " + arr );
+  }
+
   gulp.src( process.cwd() )
-    .pipe( 
-      gulpif( dryrun === true, rsync( {
+    .pipe( gulpif( dryrun === false, rsync( {
         recursive: true,
         destination: '../rsync-test/suncharts',
         progress: true,
         incremental: true,
         exclude: arr
-    } )
-  )
-    );
+      } ) 
+    )
+  );
 
 });
 
